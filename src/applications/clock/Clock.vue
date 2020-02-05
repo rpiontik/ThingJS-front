afabc43b96a4bc7848a092d96bc9c245dd3d38eb
R.Piontik <r.piontik@mail.ru>
2020-02-05 17:49:41 +0300

    Developing...

---
+++ src/applications/clock/Clock.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-expansion-panel>
+    <v-label>{{curr_time}}</v-label>
+  </v-expansion-panel>
+</template>
+
+<script>
+export default {
+  name: 'Clock',
+  mounted () {
+    this.$bus.$on($consts.EVENTS.UBUS_MESSAGE, (type, time) => {
+      if (type === 'show-time') {
+        this.curr_time = time;
+      }
+    });
+  },
+  data () {
+    return {
+      curr_time: 0
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/applications/clock/langs.js
@@ -0,0 +1,8 @@
+export default {
+  en: {
+    CURRENT_TIME: 'Current time'
+  },
+  ru: {
+    CURRENT_TIME: 'Текущее время'
+  }
+};
