74d64a4e2cce0fd1a1d1b679a59175e43889803e
Пионтик Роман <rip@rabota.ru>
2020-05-11 21:08:34 +0300

    Developing preinstalled application list

---
+++ deploy.c
@@ -0,0 +1,10 @@
+//
+// Created by rip on 11.05.2020.
+//
+
+#include "deploy.h"
+#include "stdbool.h"
+
+void thingjsGetExtAppList(struct st_thingjs_ext_app ** list, int * size) {
+    *size = 0;
+}
