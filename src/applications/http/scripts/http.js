f3f593829fe79953ac6d6418b09d96db84b38e6c
Пионтик Роман <rip@rabota.ru>
2020-05-19 18:18:42 +0300

    Developing http stdi component Added chunked transfer data

--- src/applications/http/scripts/http.js
+++ src/applications/http/scripts/http.js
@@ -1,11 +1,13 @@
 // http://ds1.tinyled.ru/api.php?action=getschedule&deviceid=C0RYHW9SQ2&version=1
 print('Try to http request');
+print('$res.http.M_POST=', $res.http.CT_FORM_URLENCODED);
 
 $res.http.request({
-    url: 'http://ds1.tinyled.ru/api.php?test=123&pest=321#fragment',
-    method: 'POST',
+    url: 'http://httpdump.io/dhaog',
+    method: $res.http.M_POST,
+    content_type: $res.http.CT_FORM_URLENCODED,
+    transfer_encoding: $res.http.TE_CHUNKED,
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
         'Test-Header': 'test header'
     },
     params: {
@@ -21,7 +23,7 @@ $res.http.request({
     index: 0,
     data: function () {
         this.index++;
-        if (this.index < 2) {
+        if (this.index < 3) {
             let result = '';
             for (let i = 1; i < 2; i++) {
                 result += 'test|1lksdjf l;ksdf;lkj s;djf ;lsdjf;lsjdf lskjdfl jds;l jf;sdl kjfsd;ljkslhdaslkjhdalsjkhdlaskjhdlakjshdkljash dlkas jkashdlkjah slkjdh laskjh dlk2345';
