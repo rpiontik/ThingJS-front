181b9abca3b1853c0fcdde6873ec474cbf2cf016
Пионтик Роман <rip@rabota.ru>
2020-09-24 11:52:26 +0300

    Fixing OneWire

--- src/applications/thermostat/scripts/thermostat.js
+++ src/applications/thermostat/scripts/thermostat.js
@@ -29,11 +29,13 @@ let fakeVector = 0.5;
 
 // Looking for ds18b20 sensors
 // Тут вешается при перезагрузке?
+print('BEGIN SEARCH');
 $res.ds18x20.search(function (addr) {
     if (sensor === null) {
         sensor = addr;
     }
 });
+print('END SEARCH');
 
 // Publish data to UBUS and MQTT server
 function publishState () {
@@ -94,8 +96,11 @@ $bus.on(function (event, data) {
 // Execution function
 $res.timers.setInterval(function () {
     if (sensor !== null) {
+        print('BEGIN GET TIME');
         $res.ds18x20.convert_all();
+        print('GET TIME');
         temp = $res.ds18x20.get_temp_c(sensor);
+        print('END GET TIME');
     } else { // Fake temperature
         if (temp > 99) {
             fakeVector = -0.5;
