091997888b1f2f88b7ceb5e130949a59b5265706
Пионтик Роман <rip@rabota.ru>
2020-11-18 21:16:53 +0300

    Added split method to string interface

--- src/applications/nodered/scripts/nodered.js
+++ src/applications/nodered/scripts/nodered.js
@@ -6,3 +6,7 @@ let String = $res.string;
 
 print(">>>>>>>>>>>",String.replaceAll("1234",4, ""),"<<<<<<<<<<<<<<<<");
 
+let items = String.split({}.a,"d");
+for(let i=0; i < items.length; i++) {
+    print("Item[",i,"]=",items[i],";");
+}
