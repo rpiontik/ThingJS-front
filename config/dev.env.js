d17c3757c38f1b49fc9ca01aaf314fc2a755d09e
Пионтик Роман <rip@rabota.ru>
2021-01-28 16:11:26 +0300

    Before migration to IDF 4.1

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  //HW_DEVICE_URL: '"http://192.168.0.196"'
-  HW_DEVICE_URL: '"http://192.168.8.101"',
+  HW_DEVICE_URL: '"http://192.168.0.196"'
+  //HW_DEVICE_URL: '"http://192.168.8.101"',
   //HW_DEVICE_URL: '"http://192.168.4.1"',
 })
--- src/applications/nodered/scripts/nodered.js
+++ src/applications/nodered/scripts/nodered.js
@@ -2,7 +2,6 @@ let $r = {
     'timer': $res.timers,
     'str': $res.string
 };
-let $g = {}, $_mid = 0;
 
 let $g = {}, $_mid = 0;
 
@@ -21,82 +20,55 @@ function $$mid() {
     return JSON.stringify(++$_mid)
 }
 
-let test = function() {
-    this.a = 10;
-    return this.a;
-};
-
-print("in function=", test(), "direct ", test.a);
-
 let $$nd = {
-    "rev": "0d2abdbe8261ad4b1aaa28d70cd5b307", "2": function ($i, $c) {
+    "rev": "42357a613c62ce0c7d52fd21c9a8e99e", "3": function ($i, $c) {
         ({
-            "$c": $c, "$n": $$nop, "$e": function ($i) {
+            "$c": $c, "$id": "3", "$n": $$nop, "$e": function ($i) {
                 print(JSON.stringify($i));
             }
         }).$e($i);
-    }, "1": function ($i, $c) {
+    }, "2": function ($i, $c) {
         ({
-            "$c": $c, "$n": function ($i) {
+            "$c": $c, "$id": "2", "$n": function ($i) {
                 $$fnd($i[0]) && $$nd["3"]($$cpy($i[0]), this.$c);
+                $$fnd($i[1]) && $$nd["4"]($$cpy($i[1]), this.$c);
             }, "$e": function ($i) {
-                $i["payload"] = ["test1", "test2", "test3", "test4", "test4"];
-                this.$n([$i]);
+                let res = [];
+                let els = true;
+                if ($i["topic"] === "topic1") {
+                    res[0] = $i;
+                    els = false;
+                }
+                if ($i["topic"] === "topic2") {
+                    res[1] = $i;
+                    els = false;
+                }
+                this.$n(res);
             }
         }).$e($i);
-    }, "3": function ($i, $c) {
+    }, "4": function ($i, $c) {
         ({
-            "$c": $c, "$n": function ($i) {
-                $$fnd($i[0]) && $$nd["2"]($$cpy($i[0]), this.$c);
+            "$c": $c, "$id": "4", "$n": function ($i) {
+                $$fnd($i[0]) && $$nd["6"]($$cpy($i[0]), this.$c);
             }, "$e": function ($i) {
-                this.mki = function ($i, pl, t, k, i, c) {
-                    $i.payload = pl;
-                    $i.parts = {"id": this.mid, "type": t};
-                    if (t === "string") {
-                        $i.parts.index = $g["_spt_4c424b17.dc4d04"]++
-                    } else {
-                        $i.parts.index = i;
-                        $i.parts.key = k;
-                        $i.parts.count = c
-                    }
-                    return $i
-                };
-                let tp = typeof $i.payload;
-                this.mid = $$mid();
-                if (tp === "string") {
-                    let ch = "|", ims = $r.str.split($i.payload, ch), ln = ims.length;
-                    for (let f = 0; f < ln; f++) {
-                        $i = this.mki($i, ims[f], "string", f, f, ln);
-                        $i.parts.ch = ch;
-                        this.$n([$i])
-                    }
-                } else if ((tp === "array") || ($i.payload["length"])) {
-                    tp = "array";
-                    let pl = $$cpy($i.payload), seg = [], ind = 0,
-                        cnt = JSON.parse($r.str.split(pl.length / 2 + 0.5, ".")[0]);
-                    for (let k in pl) {
-                        seg.push(pl[k]);
-                        if (seg.length >= 2) {
-                            $i = this.mki($i, seg, tp, undefined, ind++, cnt);
-                            $i.parts.len = 2;
-                            this.$n([$i]);
-                            seg = [];
-                        }
-                    }
-                    if (seg.length > 0) {
-                        $i = this.mki($i, seg, tp, undefined, ind, cnt);
-                        $i.parts.len = 2;
-                        this.$n([$i]);
-                    }
-                } else if (tp === "object") {
-                    let ln = 0, i = 0, pl = $$cpy($i.payload);
-                    for (let k in pl) ln++;
-                    for (let k in pl) {
-                        $i = this.mki($i, pl[k], tp, k, i++, ln);
-                        $i["key"] = k;
-                        this.$n([$i])
-                    }
-                }
+                this.$n([(function (msg) {
+                    msg = {
+                        "name": "Chris",
+                        "value": 10000,
+                        "taxed_value": 10000 - (10000 * 0.4),
+                        "in_ca": true
+                    };
+                    return msg;
+                })($i)]);
+            }
+        }).$e($i);
+    }, "6": function ($i, $c) {
+        ({
+            "$c": $c, "$id": "6", "$n": function ($i) {
+                $$fnd($i[0]) && $$nd["3"]($$cpy($i[0]), this.$c);
+            }, "$e": function ($i) {
+                $i["payload"] = $r.str.mustache("Hello {{name}}\nYou have just won {{value}} dollars!\n{{#in_ca}}\nWell, {{taxed_value}} dollars, after taxes.\n{{/in_ca}}", $i);
+                this.$n([$i]);
             }
         }).$e($i);
     }
@@ -104,16 +76,32 @@ let $$nd = {
 (function () {
     let $c = {};
     ({
-        "$c": $c, "$n": function ($i) {
-            $$fnd($i[0]) && $$nd["1"]($$cpy($i[0]), this.$c);
+        "$c": $c, "$id": "1", "$n": function ($i) {
+            $$fnd($i[0]) && $$nd["2"]($$cpy($i[0]), this.$c);
         }, "$e": function ($i) {
             this.inject = function () {
-                this.$n([{"test": "pest2", "topic": "123"}]);
+                this.$n([{"payload": "test2", "topic": "topic1"}]);
             };
-            $r.timer.setTimeout(function (scope) {
+            let scope;
+            $r.timer.setInterval(function (scope) {
                 scope.inject();
-            }, 100, this);
+            }, 1000, scope || this);
         }
     }).$e({})
 })();
-$g["_spt_4c424b17.dc4d04"] = 0;
+(function () {
+    let $c = {};
+    ({
+        "$c": $c, "$id": "5", "$n": function ($i) {
+            $$fnd($i[0]) && $$nd["2"]($$cpy($i[0]), this.$c);
+        }, "$e": function ($i) {
+            this.inject = function () {
+                this.$n([{"payload": "pest", "topic": "topic2"}]);
+            };
+            let scope;
+            $r.timer.setInterval(function (scope) {
+                scope.inject();
+            }, 1000, scope || this);
+        }
+    }).$e({})
+})();
